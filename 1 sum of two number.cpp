e14ddb57c0b61a56f9bf267796285da33ee7e97b
Anchal Singh <83942897+anchalsinghrajput@users.noreply.github.com>
2021-07-21 01:01:57 +0530

    Update 1 sum of two number.cpp

--- 1 sum of two number.cpp
+++ 1 sum of two number.cpp
@@ -1,3 +1,5 @@
+// direct
+
 #include <iostream>
 using namespace std;
 int main() 
@@ -7,5 +9,25 @@ int main()
     cin>>a>>b;
     sum=a+b;
     cout<<"Sum of two numbers is : "<<sum;
+    return 0;
+}
+
+//using function
+
+#include <iostream>
+using namespace std;
+int sum(int a, int b)
+{
+    int sum;
+    sum=a+b;
+    return sum;
+}
+
+int main() 
+{
+    int a ,b;
+    cout <<"Enter two nunbers : ";
+    cin>>a>>b;
+    cout<<"Sum of two numbers is : "<<sum(a,b);
     return 0;
 }
