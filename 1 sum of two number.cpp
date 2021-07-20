b3692b102e883a8bd2cd3838befcfc2445ad8c8d
Anchal Singh <83942897+anchalsinghrajput@users.noreply.github.com>
2021-07-21 00:56:11 +0530

    Create 1 sum of two number.cpp

---
+++ 1 sum of two number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int a ,b,sum;
+    cout <<"Enter two nunbers : ";
+    cin>>a>>b;
+    sum=a+b;
+    cout<<"Sum of two numbers is : "<<sum;
+    return 0;
+}
