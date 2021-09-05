750be59f8f4d2b6ee9be95a3dbb59667d79abae8
Anchal Singh <83942897+anchalsinghrajput@users.noreply.github.com>
2021-09-05 23:05:43 +0530

    Update simple pattern.cpp

--- patterns/simple pattern.cpp
+++ patterns/simple pattern.cpp
@@ -160,3 +160,36 @@ int main()
 
     return 0;
 }
+
+/*
+n = 5
+*
+**
+***
+****
+*****
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int n;
+    cout<<"Enter n ";
+    cin>>n;
+    int i=1;
+    while(i<=n)
+    {
+        int j=1;
+        while(j<=i)
+        {
+            cout<<"*";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+
+    return 0;
+}
