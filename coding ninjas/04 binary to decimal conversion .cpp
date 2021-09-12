d28234aef263a8e43193cbd4aa4c6826c1577cb3
Anchal Singh <83942897+anchalsinghrajput@users.noreply.github.com>
2021-09-12 12:57:02 +0530

    Create 04 binary to decimal conversion .cpp

---
+++ coding ninjas/04 binary to decimal conversion .cpp
@@ -0,0 +1,36 @@
+/*
+Binary to decimal
+Given a binary number as an integer N, convert it into decimal and print.
+Input format :
+An integer N in the Binary Format
+Output format :
+Corresponding Decimal number (as integer)
+Constraints :
+0 <= N <= 10^9
+Sample Input 1 :
+1100
+Sample Output 1 :
+12
+Sample Input 2 :
+111
+Sample Output 2 :
+7
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int rem,dec=0,p=0;
+    while(n!=0)
+    {
+        rem=n%10;
+        dec=dec+pow(2,p)*rem;
+        n=n/10;
+        p++;
+    }
+    cout<<dec;
+    return 0;
+}
