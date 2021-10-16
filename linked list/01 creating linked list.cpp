a4a3f8d6c29ae54002029a4593acbc38120a70c2
Anchal Singh <83942897+anchalsinghrajput@users.noreply.github.com>
2021-10-16 22:22:14 +0530

    Update 01 creating linked list.cpp

--- linked list/01 creating linked list.cpp
+++ linked list/01 creating linked list.cpp
@@ -15,9 +15,18 @@ class node
 
 int main()
 {
+//     STATICALLY CREATING LINKED LIST
     node n1(1);                 // creating first node
+    node *head=&n1;
     node n2(2);                 // creating second node
-    n1.next=&n2;
+    n1.next=&n2;                 //attaching two nodes
     cout<<"Elements in linked list are "<<n1.data<<" "<<n2.data<<endl;  //displying data
+    
+//     DYNAMICALLY CREATING LINKED LIST
+    node *n3 = new node(10);
+    node *n4 = new node(20);
+    n3 -> next = n4;
+    
+    cout<<n3->data<<" "<<n4->data;
     return 0;
 }
